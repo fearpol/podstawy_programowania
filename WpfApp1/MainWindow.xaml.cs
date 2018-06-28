e23d54200c82daa84f2c151399960ec3466be4d8
Fnearpol <Fnearpol@DESKTOP-0H3JFT6>
2018-06-28 19:52:32 +0200

    add lib

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Double;
+
 
 namespace WpfApp1
 {
@@ -23,6 +14,29 @@ namespace WpfApp1
 		public MainWindow()
 		{
 			InitializeComponent();
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			Matrix<double> A = DenseMatrix.OfArray(new double[,] {
+				{
+				Double.Parse(cell1.Text), Double.Parse(cell2.Text), Double.Parse(cell3.Text), Double.Parse(cell4.Text), Double.Parse(cell5.Text)
+				},
+				{
+				Double.Parse(cell6.Text), Double.Parse(cell7.Text), Double.Parse(cell8.Text), Double.Parse(cell9.Text), Double.Parse(cell10.Text)
+				},
+				{
+				Double.Parse(cell11.Text), Double.Parse(cell12.Text), Double.Parse(cell13.Text), Double.Parse(cell14.Text), Double.Parse(cell15.Text)
+				},
+				{
+				Double.Parse(cell16.Text), Double.Parse(cell17.Text), Double.Parse(cell18.Text), Double.Parse(cell19.Text), Double.Parse(cell20.Text)
+				},
+				{
+				Double.Parse(cell21.Text), Double.Parse(cell22.Text), Double.Parse(cell23.Text), Double.Parse(cell24.Text), Double.Parse(cell25.Text)
+				}
+			});
+
+			result1.Text = A.Determinant().ToString();
 		}
 	}
 }
